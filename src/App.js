69011b1add27ca4315cd93e190dc6cc5df45c80f
jkuenzinger <jkuenzinger228@gmail.com>
2020-01-22 15:19:24 -0800

    basic structure laid out

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
+import PhotoPage from "./components/PhotoPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+   <h1> Title </h1>
+   <PhotoPage/>
     </div>
   );
 }
---
+++ src/components/PhotoPage.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react";
+import PhotoCard from "./PhotoCard";
+import axios from 'axios';
+
+ function PhotoPage(){
+    const [photo, setPhoto] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://api.nasa.gov/planetary/apod?f99FqFapxKgObJJoh7oJ6klaCAdecegMpTaBdItk")
+          .then(response => {
+            console.log(response.data);
+            setPhoto(response.data);
+          })
+          .catch(error => {
+            console.log("the data was not returned", error);
+     });
+       }, []);
+      return (
+          <div>
+              <PhotoCard
+                  key={photo.date}
+                  date={photo.date}
+                  description={photo.explanation}
+                  photo={photo.url}
+                  />
+              
+          </div>
+      );
+}
+
+export default PhotoPage;
