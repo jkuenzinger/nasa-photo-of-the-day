9912d81c3955b2d3192b0dd22d3220368f215e30
jkuenzinger <jkuenzinger228@gmail.com>
2020-01-23 15:47:12 -0800

    got the project workign and aligned finally after tons of dependency issues

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import PhotoPage from "./components/PhotoPage";
 import "./App.css";
 
+
 function App() {
   return (
-    <div className="App">
-   <h1> Check out Nasa photo of the day! </h1>
+<div className="App">
+    
    <PhotoPage/>
+   
     </div>
   );
 }
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import styled from "styled-components";
+
+const PhotoDiv = styled.div`
+display:flex;
+width:100%;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+justify-content: center;
+border-style: solid 1px;
+`;
+
+const PhotoImg = styled.img`
+width: 600px;
+height: 400px;
+object-fit: scale;
+flex-shrink: 2;
+`;
+
+
 const PhotoCard = props => {
     return (
-        <div>
+        
+        <PhotoDiv>
+/>
             <h2>{props.title}</h2>
             <h3>Date: {props.date}</h3>
             <p> Description: {props.explanation}</p>
-            <img src={props.url} alt=""/>
-        </div>
+            <PhotoImg src={props.url} alt=""/>
+        </PhotoDiv>
     );
 };
 export default PhotoCard;
